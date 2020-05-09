b7a89143d0ac17c034acdcb2d352c6822dabd274
tinkrmind <tinkrmind@gmail.com>
2020-05-09 15:00:26 -0400

    Added Yeseul's code

---
+++ LED_brightness_rssi/LED_brightness_rssi.ino
@@ -0,0 +1,70 @@
+#include <ArduinoBLE.h>
+
+BLEService ledService("19B10000-E8F2-537E-4F6C-D104768A1214"); // BLE LED Service
+
+// BLE LED Switch Characteristic - custom 128-bit UUID, read and writable by central
+BLEByteCharacteristic switchCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
+
+const int ledPin = 3; // pin to use for the LED
+
+void setup() {
+  Serial.begin(9600);
+//  while (!Serial);
+
+  // set LED pin to output mode
+  pinMode(ledPin, OUTPUT);
+
+  // begin initialization
+  if (!BLE.begin()) {
+    //Serial.println("starting BLE failed!");
+
+    while (1);
+  }
+
+  // set advertised local name and service UUID:
+  BLE.setLocalName("LED");
+  BLE.setAdvertisedService(ledService);
+
+  // add the characteristic to the service
+  ledService.addCharacteristic(switchCharacteristic);
+
+  // add service
+  BLE.addService(ledService);
+
+  // set the initial value for the characeristic:
+  switchCharacteristic.writeValue(0);
+
+  // start advertising
+  BLE.advertise();
+
+  //Serial.println("BLE LED Peripheral");
+}
+
+void loop() {
+  // listen for BLE peripherals to connect:
+  BLEDevice central = BLE.central();
+
+  // if a central is connected to peripheral:
+  if (central) {
+    
+    // while the central is still connected to peripheral:
+    while (central.connected()) {
+      
+      // if the remote device wrote to the characteristic,
+      // use the rssi value to control the LED brightness:      
+      
+      int rssiToBrightness = map(BLE.rssi(), -50, -30, 0, 255);      
+      rssiToBrightness = constrain(rssiToBrightness, 0, 255);
+      
+      Serial.print(BLE.rssi());
+      Serial.print(", ");     
+      Serial.println(rssiToBrightness);
+      analogWrite(ledPin, rssiToBrightness);
+    } 
+    analogWrite(ledPin, 0);
+
+    // when the central disconnects, print it out:
+    Serial.print(F("Disconnected from central: "));
+    Serial.println(central.address());
+  }
+}
